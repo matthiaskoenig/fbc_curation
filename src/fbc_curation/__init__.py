210c66a0e8dd8821a4d74bfbb72f269bfe16eb15
Matthias Koenig <konigmatt@googlemail.com>
2020-10-29 23:59:29 +0100

    Bump version: 0.1.3 → 0.1.4

--- src/fbc_curation/__init__.py
+++ src/fbc_curation/__init__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 __software__ = "fbc_curation"
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __citation__ = "https://doi.org/10.5281/zenodo.3708271"
 
 EXAMPLE_PATH = Path(__file__).parent / "examples"
