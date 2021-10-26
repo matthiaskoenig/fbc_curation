f610eb16bea43d5cadb89c2621ca9d8b174b9936
Matthias Koenig <konigmatt@googlemail.com>
2021-10-26 02:37:23 +0200

    fixed celery connection

--- docker-purge.sh
+++ docker-purge.sh
@@ -2,9 +2,6 @@
 docker-compose -f docker-compose-production.yml down --volumes --rmi local
 
 # make sure containers are removed (if not running)
-docker container rm -f fbc_curation_nginx_1
-docker container rm -f fbc_curation_backend_1
-docker container rm -f fbc_curation_frontend_1
 docker container rm -f frog_nginx_1
 docker container rm -f frog_backend_1
 docker container rm -f frog_frontend_1
@@ -12,11 +9,7 @@ docker container rm -f frog_redis_1
 docker container rm -f frog_worker_1
 docker container rm -f frog_dashboard_1
 
-
 # make sure images are removed
-docker image rm -f fbc_curation_nginx:latest
-docker image rm -f fbc_curation_backend:latest
-docker image rm -f fbc_curation_frontend:latest
 docker image rm -f frog_nginx:latest
 docker image rm -f frog_backend:latest
 docker image rm -f frog_frontend:latest
