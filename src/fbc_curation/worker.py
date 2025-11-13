70cbf4ec3ad979ddb115909b100d16cc4a84be4a
Matthias Koenig <konigmatt@googlemail.com>
2025-11-14 00:40:33 +0100

    clean separation of celery worker

--- src/fbc_curation/worker.py
+++ src/fbc_curation/worker.py
@@ -3,7 +3,6 @@
 Here the tasks are defined.
 """
 
-import os
 import tempfile
 import time
 from pathlib import Path
@@ -21,10 +20,6 @@ from fbc_curation.frog import FrogReport
 
 
 logger = log.get_logger(__name__)
-
-
-# storage of data on server, only relevant for server
-FROG_STORAGE = "/frog_data"
 
 
 def run_frog(source_path: Path, omex_path: Path) -> None:
@@ -47,8 +42,6 @@ def run_frog(source_path: Path, omex_path: Path) -> None:
 def frog_task(
     source_path_str: str,
     omex_path_str: Optional[str] = None,
-    input_is_temporary: bool = False,
-    frog_storage_path_str: str = FROG_STORAGE,
 ) -> Dict[str, Any]:
     """Run FROG task and create JSON for omex path.
 
@@ -59,68 +52,56 @@ def frog_task(
     """
     logger.info(f"Loading '{source_path_str}'")
 
-    try:
-        omex_path = Path(source_path_str)
-        if not omex_path.exists():
-            raise IOError(f"Path does not exist: '{omex_path}'")
-        if not omex_path.is_file():
-            raise IOError(f"Path is not a file: '{omex_path}'")
+    omex_path = Path(source_path_str)
+    if not omex_path.exists():
+        raise IOError(f"Path does not exist: '{omex_path}'")
+    if not omex_path.is_file():
+        raise IOError(f"Path is not a file: '{omex_path}'")
 
-        # move data for task
+    # move data for task
 
-        if Omex.is_omex(omex_path):
-            omex = Omex().from_omex(omex_path)
-        else:
-            # Path is SBML we create a new archive
-            omex = Omex()
-            omex.add_entry(
-                entry_path=omex_path,
-                entry=ManifestEntry(
-                    location=f"./{omex_path.name}", format=EntryFormat.SBML, master=True
-                ),
-            )
+    if Omex.is_omex(omex_path):
+        omex = Omex().from_omex(omex_path)
+    else:
+        # Path is SBML we create a new archive
+        omex = Omex()
+        omex.add_entry(
+            entry_path=omex_path,
+            entry=ManifestEntry(
+                location=f"./{omex_path.name}", format=EntryFormat.SBML, master=True
+            ),
+        )
 
-        content = {"manifest": omex.manifest.model_dump(), "frogs": {}}
+    content = {"manifest": omex.manifest.model_dump(), "frogs": {}}
 
-        # Add FROG JSON for all SBML files
-        entry: ManifestEntry
-        for entry in omex.manifest.entries:
-            if entry.is_sbml():
-                # TODO: check that SBML model with FBC information
+    # Add FROG JSON for all SBML files
+    entry: ManifestEntry
+    for entry in omex.manifest.entries:
+        if entry.is_sbml():
+            # TODO: check that SBML model with FBC information
 
-                report_dict = {}
-                for curator_key in ["cobrapy", "cameo"]:
-                    sbml_path: Path = omex.get_path(entry.location)
-                    report: FrogReport = _frog_for_sbml(
-                        source=sbml_path, curator_key=curator_key
-                    )
+            report_dict = {}
+            for curator_key in ["cobrapy", "cameo"]:
+                sbml_path: Path = omex.get_path(entry.location)
+                report: FrogReport = _frog_for_sbml(
+                    source=sbml_path, curator_key=curator_key
+                )
 
-                    # add FROG files to archive
-                    report.add_to_omex(
-                        omex, location_prefix=f"./{FROG_PATH_PREFIX}/{curator_key}/"
-                    )
+                # add FROG files to archive
+                report.add_to_omex(
+                    omex, location_prefix=f"./{FROG_PATH_PREFIX}/{curator_key}/"
+                )
 
-                    # add JSON to response
-                    report_dict[curator_key] = report.model_dump()
+                # add JSON to response
+                report_dict[curator_key] = report.model_dump()
 
-                # store all reports for SBML entry
-                content["frogs"][entry.location] = report_dict
+            # store all reports for SBML entry
+            content["frogs"][entry.location] = report_dict
 
-        # save archive for download
-        if not omex_path_str:
-            raise ValueError(
-                "The 'omex_path_str' argument must be set (if not executed "
-                "within a celery Task)."
-            )
-
-        omex_path = Path(omex_path_str)
-        console.rule("Write OMEX", style="white")
-        omex.to_omex(omex_path=omex_path)
-
-    finally:
-        # cleanup temporary files for celery
-        if input_is_temporary:
-            os.remove(source_path_str)
+    # save archive for download
+    omex_path = Path(omex_path_str)
+    console.rule("Write OMEX", style="white")
+    omex.to_omex(omex_path=omex_path)
 
     return content
 
